6d72d591b74dc9dc82d529465ec00f1b43a58064
Papa-Shrim <chrisherron18@gmail.com>
2026-02-04 13:38:28 -0700

    introduction of main_menu to git

---
+++ class/p_environment/MainMenu.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+
+public partial class MainMenu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("printedq");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void _on_play_pressed()
+	{
+		GetTree().ChangeSceneToNode(GetNode("res://class/p_environment/world.tscn"));
+	}
+
+	public async void _on_quit_pressed()
+    {
+		GetTree().Quit();
+		GD.Print("printed");
+	}
+}
