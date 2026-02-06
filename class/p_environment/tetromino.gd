a83725e4347a3b5d68ab5110466ce80bbd2de4d2
Papa-Shrim <chrisherron18@gmail.com>
2026-02-06 13:52:22 -0700

    scaled and placed values added to tetris things

--- class/p_environment/tetromino.gd
+++ class/p_environment/tetromino.gd
@@ -30,6 +30,7 @@ func _ready() -> void:
 	print(tetromino_type)
 	cur_scale = $".".get_parent().scale.x
 	generated()
+	print(get_parent().get_meta("placed"))
 	#$"1/sprite1/Sprite2D".texture =flower1
 
 
@@ -105,20 +106,19 @@ func disabling(path: Node):
 
 
 func _input(event):
-	if Input.is_action_just_pressed("click"):
+	if Input.is_action_pressed("click") and get_parent().get_meta("placed") == false:
 		print("clicked, ",event.position,", ",", ",global_position,", ",$".".get_parent().scale.x)
 		if (event.position.x >= position.x and event.position.x <= (position.x + (256 * $".".get_parent().scale.x)))and(event.position.y >= position.y and event.position.y <= (position.y + (512 * $".".get_parent().scale.y))):
 			focused = true
 			print(focused)
 			while focused:
-				global_position = get_global_mouse_position() - Vector2(128.0 * 0.5,128.0 * 0.5)
-				print("While")
+				global_position = get_global_mouse_position() - Vector2(128.0 * cur_scale,128.0 * cur_scale)
 				if Input.is_action_just_released("click"):#Input.is_action_just_pressed("click"):
 					focused = false
 					print(focused)
 				await get_tree().create_timer(1.0/120).timeout
 			if focused == false:
-				print("if")
-				position = position.snapped(Vector2(128,128))
+				position = position.snapped(Vector2(128*cur_scale,128*cur_scale))
+				get_parent().set_meta("placed",true)
 		#print("Mouse Click/Unclick at: ", event.position)
 		#print((event.position.x >= position.x and event.position.x <= (position.x + 256))and(event.position.y >= position.y and event.position.y <= (position.y + 512)))
