b153010c5b8a9566f6a9e89448f1365a12900d6f
Gamedagamez <gamedagames@gmail.com>
2026-02-12 13:19:01 -0700

    DontBreakDontBreakDontBreak

--- class/p_environment/Llevel.cs
+++ class/p_environment/Llevel.cs
@@ -1,17 +1,23 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
 
 public partial class Llevel : Node2D
 {
+	PackedScene tetro = GD.Load<PackedScene>("res://class/p_environment/tetromino.tscn");
 	private Node2D parent ;
 	private int stage ;
 	private int minimum ;
-	private int reward ;
+	private int reward = 3;
 	private int Percent = 0;
+	private Node2D TetroMain1 ;
+	private Node2D TetroMain2 ;
+	private Node2D TetroMain3 ;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		parent = (Node2D)GetParent().GetParent() ;
+		parent = (Node2D)GetParent() ;
 		GD.Print(parent.Name);
 		stage = (int)parent.GetMeta("Stage"); 
 		minimum = (int)Math.Round((Math.Pow(2,stage)/10)+Math.Pow(stage,2)+10) ;
@@ -25,7 +31,21 @@ public partial class Llevel : Node2D
 	{
 		if(Percent >= minimum)
 		{
-			
+			parent.SetMeta("Currency",(int)parent.GetMeta("Currency")+reward);
 		}
 	}
+
+	public void spawnTetromino(Marker2D used)
+	{
+		Control instance = (Control)tetro.Instantiate();
+		AddChild(instance);
+		instance.Position = used.Position ;
+		instance.Rotation = Mathf.DegToRad(90);
+		instance.Scale = new Vector2(0.2f,0.2f);
+		TetroMain1.scale
+	}
+	public void _on_tetro_1_signal_button_down(){spawnTetromino(GetNode<Marker2D>("Marker2D"));}
+	public void _on_tetro_2_signal_button_down(){spawnTetromino(GetNode<Marker2D>("Marker2D2"));}
+	public void _on_tetro_3_signal_button_down(){spawnTetromino(GetNode<Marker2D>("Marker2D3"));}
+	
 }
--- class/p_environment/World.cs
+++ class/p_environment/World.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class World : Node2D
 {
+	PackedScene shop = GD.Load<PackedScene>("res://class/gc_shop/PShopInterface.tscn");
+	PackedScene level = GD.Load<PackedScene>("res://class/p_environment/Llevel.tscn");
 	public int currency = 0;
 	public int stage = 0;
 	// Called when the node enters the scene tree for the first time.
@@ -13,5 +15,18 @@ public partial class World : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void _on_child_exiting_tree(Node node)
+	{
+		if(node.Name == "LLevel"){
+			Node instance = shop.Instantiate();
+			AddChild(instance);
+		}
+		else if(node.Name == "Shop")
+		{
+			Node instance = level.Instantiate();
+			AddChild(instance);
+		}
 	}
 }
